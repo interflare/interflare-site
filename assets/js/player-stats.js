61d28e3040b314231ce3b8aa53058a4947a04f14
Matt Crook <mcro591@aucklanduni.ac.nz>
2017-12-22 23:50:48 +1300

    autoload and cursor player list

--- assets/js/player-stats.js
+++ assets/js/player-stats.js
@@ -12,14 +12,33 @@ class PlayerStats {
             _base: 'https://us-central1-interflare-minecraft.cloudfunctions.net/',
             worlds: 'gameinfo-worlds',
             players: 'gameinfo-players?cx={cx}',
-            blockcounts: 'gameinfo-blockcounts?pid={pid}&wid={wid}'
         };
 
-        //this.initializeLayout();
+        this.players = [];
+        this.worlds = [];
     }
 
-    initializeLayout() {
-        this.base.html(``);
+    getPlayerList(next, cursor = null) {
+        var uri = this.endpoints._base + this.endpoints.players;
+        uri = uri.replace('{cx}', cursor ? cursor : '');
+
+        console.info(`getPlayerList(): connecting to ${uri}`);
+        let xhr_start = new Date();
+
+        $.getJSON(uri)
+            .done((payload) => {
+                let xhr_diff = new Date() - xhr_start;
+                console.info(`getPlayerList(): completed xhr in ${xhr_diff}ms`);
+                
+                this.players.push.apply(payload.players);
+                next(null, payload);
+            })
+            .fail((xhr, txtStatus, err) => {
+                let xhr_diff = new Date() - xhr_start;
+                console.info(`getPlayerList(): failed (${xhr_diff}ms) - ${err}`);
+
+                next(err, null);
+            });
     }
 
     updateLayout() {

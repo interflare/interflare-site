bf4ebc1b656f8335fe89d9866a872a824d2d07f2
Matt Crook <mattlcrook@icloud.com>
2020-04-13 16:16:30 +1200

    add timestamp to status response contract

--- api/Contracts/ServerStatusResponse.cs
+++ api/Contracts/ServerStatusResponse.cs
@@ -1,14 +1,20 @@
-using System.Text.Json.Serialization;
+using System;
 
 namespace InterFlare.Api.Contracts
 {
     public class ServerStatusResponse
     {
+        public DateTimeOffset Updated { get; }
         public ServerStatus Status { get; set; }
         public long Latency { get; set; }
         public string Version { get; set; }
         public string Motd { get; set; }
         public PlayerCounts Players { get; set; }
+
+        public ServerStatusResponse()
+        {
+            Updated = DateTimeOffset.UtcNow;
+        }
     }
 
     public enum ServerStatus
